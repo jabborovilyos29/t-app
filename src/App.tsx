1766571d46bbdb42bca198ed8ccce4090b0de189
Джабборов Илёс <dzabboroviles@MacBook-Pro-Dzabborov.local>
2025-08-28 11:17:26 +0500

    refactor/remove-title

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-      <div>
-        <h1>bkap kunti</h1>
-      </div>
-  )
+    <div>
+      <h1>test app</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
